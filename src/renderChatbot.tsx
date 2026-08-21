755c78c22a1519ab56c98ce4610fc8f7ef5c38dd
Naveen S <huesh180@gmail.com>
2026-08-21 17:25:40 +0530

    v1.12.0 - React Canvas UI live in extension

--- src/renderChatbot.tsx
+++ src/renderChatbot.tsx
@@ -1,8 +1,7 @@
-﻿import React from 'react';
+import React from 'react';
 import { createRoot } from 'react-dom/client';
 import { flushSync } from 'react-dom';
 import { Chatbot } from './components/ui/demo';
-import '../src/styles/globals.css';
 
 export function mountChatbotSync(container: HTMLElement) {
   const root = createRoot(container);
